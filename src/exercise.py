1b77161f406bb625cef12eaf86bb84b9ec9e83a4
saidgar <sebast45ian@gmail.com>
2020-10-04 14:18:44 +0200

    Attempt 1.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
-
+    print("I will tell you a story, but I need some information first.")
+    print("What is the main character called?")
+    name = input()
+    print("What is their job?")
+    job = input()
+    print("Here is the story:")
+    print("Once upon a time there was " + name + ", who was a " + job + ".")
+    print("On the way to work, " + name + " reflected on life.")
+    print("Perhaps " + name + " will not be a " + job + " forever.")
 if __name__ == '__main__':
     main()
